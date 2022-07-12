80bec875a02093a7e00e9a9b20ae580b11159910
André Erdmann <dywi@mailerd.de>
2022-07-12 22:06:37 +0200

    collections/base, cleanup hook: log action

--- collections/base/hooks/customize/89-cleanup.sh
+++ collections/base/hooks/customize/89-cleanup.sh
@@ -2,11 +2,13 @@
 # Cleanup just before finalizing the image
 
 # remove temporary mmdebstrap dpkg.conf, apt.conf
+print_action "Clean up temporary configuration files"
 autodie rm -- "${TARGET_ROOTFS:?}/etc/dpkg/dpkg.cfg.d/99mmdebstrap-local"
 autodie rm -- "${TARGET_ROOTFS:?}/etc/apt/apt.conf.d/99mmdebstrap-local"
 
 # remove cached apt files
 #  (only useful when creating images from TARGET_ROOTFS directly)
+print_action "Remove cached apt files"
 autodie target_chroot apt-get clean
 autodie find \
     "${TARGET_ROOTFS:?}/var/cache/apt" \
