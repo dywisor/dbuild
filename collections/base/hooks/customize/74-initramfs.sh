3f2c98c4bd88ceac0c9a20afe3f249def471f470
André Erdmann <dywi@mailerd.de>
2022-10-02 12:25:52 +0200

    collections/base: update initramfs, dep on kernel
    Do not update the initramfs if no kernel image has been configured.

--- collections/base/hooks/customize/74-initramfs.sh
+++ collections/base/hooks/customize/74-initramfs.sh
@@ -8,8 +8,13 @@
         die "Failed to load update-initramfs environment file"
 
     if feat_all "${WANT_UPDATE_INITRAMFS:-0}"; then
-        print_action "Regenerate initramfs"
-        autodie target_chroot update-initramfs -u -k all
+        if feat_all "${OFEAT_KERNEL_IMAGE:-0}"; then
+            print_action "Regenerate initramfs"
+            autodie target_chroot update-initramfs -u -k all
+
+        else
+            print_info "Not updating initramfs, no kernel image configured."
+        fi
 
     else
         print_info "Not updating initramfs, not requested."
