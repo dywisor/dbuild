40e24dec56a2ebe5c3e7f9a7fdfb0592037fde81
André Erdmann <dywi@mailerd.de>
2023-02-10 00:32:49 +0100

    collections/bootable: fix grub debconf

--- collections/bootable/uefi/hooks/essential/05-grub_debconf.sh
+++ collections/bootable/uefi/hooks/essential/05-grub_debconf.sh
@@ -3,5 +3,5 @@
 print_action "Configure grub package debconf for UEFI targets"
 autodie target_debconf << EOF
 grub-efi-amd64 grub2/update_nvram boolean false
-grub-efi-amd64 grub2/foce_efi_extra_removable boolean true
+grub-efi-amd64 grub2/force_efi_extra_removable boolean true
 EOF
