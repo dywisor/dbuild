cc74991a9127d131548b5d87bd81b124a36fc67d
André Erdmann <dywi@mailerd.de>
2023-02-10 00:20:54 +0100

    collections/bootable: optionally generate uuid for /boot

--- collections/bootable/base/hooks/setup/05-disk_uuid.sh
+++ collections/bootable/base/hooks/setup/05-disk_uuid.sh
@@ -1,3 +1,8 @@
 #!/bin/sh
 print_action "Generate UUID for rootfs"
 uuidgen > "${DBUILD_STAGING_TMP:?}/uuid.rootfs" || die
+
+if feat_all "${OFEAT_SEPARATE_BOOT:-0}"; then
+    print_action "Generate UUID for /boot"
+    uuidgen > "${DBUILD_STAGING_TMP:?}/uuid.boot" || die
+fi
