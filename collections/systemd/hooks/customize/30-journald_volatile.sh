0683486078ae59604b686c9e7bc5d783ab2cd9b7
André Erdmann <dywi@mailerd.de>
2023-09-21 19:28:19 +0200

    collections/systemd: let journald write to volatile storage
    ... when rsyslog writes to disk

---
+++ collections/systemd/hooks/customize/30-journald_volatile.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+# Lets systemd-journald write to volatile storage only
+# if another syslog daemon has been installed.
+#
+
+gen_journald_volatile() {
+cat << EOF
+[Journal]
+Storage=volatile
+EOF
+}
+
+
+if feat_all "${OFEAT_RSYSLOG:-0}"; then
+    print_action "Configure volatile storage for systemd-journald"
+
+    autodie dofile \
+        "${TARGET_ROOTFS}/etc/systemd/journald.conf.d/volatile.conf" \
+        0644 "0:0" \
+        gen_journald_volatile
+fi
