ad9c25825e72146afb11aa4eed360ebb8b4d5a60
André Erdmann <dywi@mailerd.de>
2024-03-07 21:40:08 +0100

    fix env.d (0080ec069142bccf1dc32b1759f21b58c90bdc25)

--- collections/base/hooks/customize/73-envd.sh
+++ collections/base/hooks/customize/73-envd.sh
@@ -1,9 +1,11 @@
 #!/bin/sh
 # Generate /etc/environment from env.d snippets
 
+print_action "Generate /etc/environment"
+
 # also use existing environment file from target rootfs
 if [ -r "${TARGET_ROOTFS:?}/etc/environment" ]; then
-    dbuild_envd_push 00-default "${TARGET_ROOTFS:?}/etc/environment"
+    dbuild_envd_push 00-default < "${TARGET_ROOTFS:?}/etc/environment"
 fi
 
 target_write_to_file /etc/environment "0644" "0:0" dbuild_envd_cat
